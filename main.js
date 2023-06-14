833c1d4cdcb252e8814617bf49c7bf3d6363d4b3
Niek1610 <Niek.huisman2002@gmail.com>
2023-06-14 21:01:28 +0200

    updating

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ function scrollToSection(sectionId) {
     section.scrollIntoView({ behavior: 'smooth', block: 'start' });
   }
 
+
+  const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting){
+                entry.target.classList.add("sectionShow")
+            } else {
+                entry.target.classList.remove("sectionShow")
+            }
+        } )
+  })
+
+const section = document.querySelectorAll(".section")
+section.forEach((el) => observer.observe(el))
